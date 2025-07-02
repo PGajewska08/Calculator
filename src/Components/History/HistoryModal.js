37bf439530c878c4d0ebcff8c1070a8e0a216a3f
Paulina <gajewska.paulina08@gmail.com>
2025-07-02 22:17:20 +0200

    add show history action

--- src/Components/History/HistoryModal.js
+++ src/Components/History/HistoryModal.js
@@ -2,10 +2,16 @@ import styles from './HistoryModal.module.scss';
 
 const HistoryModal = ({history}) => {
     return (
+        
         <div className={styles.modal}>
+            { (history.length===0) && <p>Brak historii</p>}
             <ul>
                 {history.map((item, index) => (
-                    <li key={index}>{item}</li>
+                    <li key={index}
+                    className={index=== history.length -1 ? styles.last : ""}
+                    >
+                        {item}
+                    </li>
                 ))}
             </ul>
            
